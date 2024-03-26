09580c8038a4ee97d6da599929d14f88af04200c
den10b <den10b@yandex.ru>
2024-03-26 19:11:48 +0300

    fix

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
-
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
@@ -16,6 +15,12 @@ class Student(db.Model):
 def index(student_id):
     student = Student.query.filter_by(id=student_id).first()
     return render_template('static/index.html', student_name=student.name)
+
+
+@app.route('/INSERT')
+def index():
+    Student.name=""
+    return
 
 
 if __name__ == '__main__':
